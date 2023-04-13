78e18852503a85325c2653a5de49948fea8bc3a0
MajaMarkiewicz <markiewicz.maja@gmail.com>
2023-04-13 09:21:26 +0200

    Add input component

---
+++ components/Input.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+interface InputProps {
+    className: String;
+  }
+
+const Input: FC<InputProps> = ({ className, ...props }) => {
+  return (
+    <input
+      className={`border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full ${className}`}
+      {...props}
+    />
+  );
+};
+
+export default Input;
