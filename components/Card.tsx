190cc208baf8573726d8de5771d9afa4f416e88b
MajaMarkiewicz <markiewicz.maja@gmail.com>
2023-04-13 09:08:51 +0200

    Add card component

---
+++ components/Card.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react'
+
+const Card = ({ className, children }: PropsWithChildren<{className: String}>) => {
+  return (
+    <div className={`rounded-3xl px-10 py-4 drop-shadow-xl bg-white ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
